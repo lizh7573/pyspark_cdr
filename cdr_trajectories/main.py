59b7768c2968bf797016acc5ff35ab54c48f0fad
Liyuan Zhang <liyuan.zhang.7573@student.uu.se>
2021-10-25 22:51:53 +0200

    update

--- cdr_trajectories/main.py
+++ cdr_trajectories/main.py
@@ -3,6 +3,7 @@ Main module
 ===========
 """
 
+import os
 import pyspark.sql.functions as F
 from pyspark.sql.functions import arrays_zip
 from cdr_trajectories.mpn import mpn_data
@@ -11,7 +12,7 @@ from cdr_trajectories.ring import oneRing_data, twoRing_data, threeRing_data
 from cdr_trajectories.TM import TM
 from cdr_trajectories.OD import OD
 from cdr_trajectories.time_inhomo import time_inhomo
-from cdr_trajectories.udfs import prepare_for_plot, plot_dense, plot_sparse
+from cdr_trajectories.udfs import prepare_for_sparse_plot, prepare_for_dense_plot, plot_sparse, plot_dense
 
 trajectories = mpn_data.join(voronoi_data, ['avg_X', 'avg_Y'], how = 'inner')\
                        .orderBy(['user_id', 'timestamp'])
@@ -25,38 +26,38 @@ zeroRing_trajectories = trajectories.withColumn('neighbors', F.array('voronoi_id
 deterministic_trajectories = zeroRing_trajectories
 
 tm_0 = TM(deterministic_trajectories).make_tm()
-tm_0.toPandas().to_csv('test.csv')
-plot_sparse(prepare_for_plot(tm_0, 'updates'), 'TM_0.png', 'Transition Matrix (Deterministic)')
+plot_sparse(prepare_for_sparse_plot(tm_0, 'updates'), 'TM_0.png', 'Transition Matrix (Deterministic)')
 
 
-# class Trajectories:
 
-#     def __init__(self, df, ring):
-#         self.df = df
-#         self.ring = ring
+class Trajectories:
 
-#     def join(self):
-#         self.df = self.df.join(self.ring, ['voronoi_id'], how = 'inner')\
-#                          .orderBy(['user_id', 'timestamp'])\
-#                          .drop('avg_X', 'avg_Y', 'neighbors', 'props')
-#         return self.df
+    def __init__(self, df, ring):
+        self.df = df
+        self.ring = ring
 
-# oneRing_trajectories = Trajectories(trajectories, oneRing_data).join()
-# twoRing_trajectories = Trajectories(trajectories, twoRing_data).join()
-# threeRing_trajectories = Trajectories(trajectories, threeRing_data).join()
+    def join(self):
+        self.df = self.df.join(self.ring, ['voronoi_id'], how = 'inner')\
+                         .orderBy(['user_id', 'timestamp'])\
+                         .drop('avg_X', 'avg_Y', 'neighbors', 'props')
+        return self.df
 
-# probabilistic_trajectories = threeRing_trajectories
+oneRing_trajectories = Trajectories(trajectories, oneRing_data).join()
+twoRing_trajectories = Trajectories(trajectories, twoRing_data).join()
+threeRing_trajectories = Trajectories(trajectories, threeRing_data).join()
 
-# tm_1 = TM(oneRing_trajectories).make_tm()
-# plot_dense(prepare_for_plot(tm_1, 'updates'), 'TM_1.png', 'Transition Matrix (One Ring)')
-# tm_2 = TM(twoRing_trajectories).make_tm()
-# plot_dense(prepare_for_plot(tm_2, 'updates'), 'TM_2.png', 'Transition Matrix (Two Rings)')
-# tm_3 = TM(probabilistic_trajectories).make_tm()
-# plot_dense(prepare_for_plot(tm_3, 'updates'), 'TM_3.png', 'Transition Matrix (Three Rings)')
+probabilistic_trajectories = threeRing_trajectories
+
+tm_1 = TM(oneRing_trajectories).make_tm()
+plot_dense(prepare_for_dense_plot(tm_1, 'updates'), 'TM_1.png', 'Transition Matrix (One Ring)')
+tm_2 = TM(twoRing_trajectories).make_tm()
+plot_dense(prepare_for_dense_plot(tm_2, 'updates'), 'TM_2.png', 'Transition Matrix (Two Rings)')
+tm_3 = TM(probabilistic_trajectories).make_tm()
+plot_dense(prepare_for_dense_plot(tm_3, 'updates'), 'TM_3.png', 'Transition Matrix (Three Rings)')
 
 
-# od = OD(probabilistic_trajectories).make_od()
-# plot_dense(prepare_for_plot(od, 'updates'), 'OD.png', 'Origin-Destination Matrix')
+od = OD(probabilistic_trajectories).make_od()
+plot_dense(prepare_for_dense_plot(od, 'updates'), 'OD.png', 'Origin-Destination Matrix')
 
 #Paremeters are subjected to change
 #tm_time = TM_OD(time_inhomo(cdr_trajectories, 6, 8).make_tm_time()).make_tm()
--- cdr_trajectories/udfs.py
+++ cdr_trajectories/udfs.py
@@ -9,13 +9,27 @@ import numpy as np
 import scipy.sparse as sparse
 import matplotlib.pyplot as plt
 
+
+
 def matrix_updates(states1, states2):
     update = [[float(el1[0]), float(el2[0]), el1[1]*el2[1]]
              for el1 in states1 for el2 in states2]
     return update
 
 
-def prepare_for_plot(data, type_):
+def prepare_for_sparse_plot(data, type_):
+
+    pd_df = data.toPandas()
+
+    data = np.array( pd_df[type_] )
+    rows = np.array( pd_df['y'].astype('int') )
+    cols = np.array( pd_df['x'].astype('int') )
+
+    A = sparse.coo_matrix((data, (rows, cols)), shape = (114, 114))
+
+    return A
+
+def prepare_for_dense_plot(data, type_):
 
     pd_df = data.toPandas()
 
@@ -26,6 +40,16 @@ def prepare_for_plot(data, type_):
     A = sparse.coo_matrix((data, (rows, cols)))
 
     return A
+
+
+def plot_sparse(matrix, fname, title):
+    plt.figure(figsize = (20, 20))
+    plt.spy(matrix, markersize = 10, alpha = 0.5)
+    plt.grid()
+    plt.xlabel("polygon", fontsize = 20)
+    plt.ylabel("polygon", fontsize = 20)
+    plt.title(title, fontsize = 30)
+    plt.savefig(os.path.join('outputs', fname))
 
 
 def plot_dense(matrix, fname, title):
@@ -39,11 +63,4 @@ def plot_dense(matrix, fname, title):
     plt.savefig(os.path.join('outputs', fname))
 
 
-def plot_sparse(matrix, fname, title):
-    plt.figure(figsize = (20, 20))
-    plt.spy(matrix, markersize = 10, alpha = 0.5)
-    plt.grid()
-    plt.xlabel("polygon", fontsize = 20)
-    plt.ylabel("polygon", fontsize = 20)
-    plt.title(title, fontsize = 30)
-    plt.savefig(os.path.join('outputs', fname))
+
